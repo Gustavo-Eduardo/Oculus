03517c672da8c00cbfc3ff86f8d3592af99feb66
zend3v <zend3v@icloud.com>
2024-03-15 00:48:37 -0700

    si

---
+++ Assets/Scripts/ColorChange.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChange : MonoBehaviour
+{
+    [SerializeField]
+    private MeshRenderer meshRenderer;
+
+    private void Awake()
+    {
+        VoiceRecognitionManager.Instance.OnStartRecording += ActivateColor;
+        VoiceRecognitionManager.Instance.OnStopRecording += DeactivateColor;
+    }
+
+    private void ActivateColor(object sender, EventArgs ev)
+    {
+        meshRenderer.material.color = Color.red;
+    }
+    private void DeactivateColor(object sender, EventArgs ev)
+    {
+        meshRenderer.material.color = Color.white;
+    }
+}
